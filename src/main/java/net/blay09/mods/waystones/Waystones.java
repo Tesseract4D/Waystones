b7eb0e00f9c01b07a76e32ca801b9c3c82a47a3a
梁弘毅 <55395193+tesseract4d@users.noreply.github.com>
2025-05-04 22:06:04 +0800

    更新构建

--- src/main/java/net/blay09/mods/waystones/Waystones.java
+++ src/main/java/net/blay09/mods/waystones/Waystones.java
@@ -6,7 +6,6 @@ import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLInterModComms;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
-import cpw.mods.fml.common.event.FMLServerStartingEvent;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.blay09.mods.waystones.block.BlockWaystone;
 import net.blay09.mods.waystones.block.TileWaystone;
@@ -21,95 +20,99 @@ import net.minecraftforge.common.ChestGenHooks;
 import net.minecraftforge.common.config.Configuration;
 import net.minecraftforge.oredict.ShapedOreRecipe;
 
-@Mod(modid = Waystones.MOD_ID, name = "Waystones", version = Waystones.VERSION)
-@SuppressWarnings("unused")
+@Mod(modid = Waystones.MOD_ID, name = Waystones.MOD_NAME, version = Tags.VERSION)
 public class Waystones {
 
 
-	/** This mod's id. */
-	public static final String MOD_ID = Tags.MODID;
-	public static final String MOD_Name = Tags.MODNAME;
+    /**
+     * This mod's id.
+     */
+    public static final String MOD_ID = "waystones";
+    public static final String MOD_NAME = "Waystones";
 
 
-	/** This mod's version. */
-	public static final String VERSION = Tags.VERSION;
+    /**
+     * This mod's version.
+     */
+    public static final String VERSION = Tags.VERSION;
 
-	@SidedProxy(serverSide = Tags.GROUPNAME + ".CommonProxy", clientSide = Tags.GROUPNAME + ".client.ClientProxy")
-	public static CommonProxy proxy;
+    @SidedProxy(serverSide = "net.blay09.mods.waystones.CommonProxy", clientSide = "net.blay09.mods.waystones.client.ClientProxy")
+    public static CommonProxy proxy;
 
-	public static BlockWaystone blockWaystone;
-	public static ItemReturnScroll itemReturnScroll;
-	public static ItemWarpStone itemWarpStone;
+    public static BlockWaystone blockWaystone;
+    public static ItemReturnScroll itemReturnScroll;
+    public static ItemWarpStone itemWarpStone;
 
-	public static Configuration configuration;
+    public static Configuration configuration;
 
-	private static WaystoneConfig config;
+    private static WaystoneConfig config;
 
-	@Mod.EventHandler
-	public void preInit(FMLPreInitializationEvent event) {
-		blockWaystone = new BlockWaystone();
-		GameRegistry.registerBlock(blockWaystone, "waystone");
-		GameRegistry.registerTileEntity(TileWaystone.class, MOD_ID + ":waystone");
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        blockWaystone = new BlockWaystone();
+        GameRegistry.registerBlock(blockWaystone, "waystone");
+        GameRegistry.registerTileEntity(TileWaystone.class, MOD_ID + ":waystone");
 
-		itemReturnScroll = new ItemReturnScroll();
-		GameRegistry.registerItem(itemReturnScroll, "warpScroll");
+        itemReturnScroll = new ItemReturnScroll();
+        GameRegistry.registerItem(itemReturnScroll, "warpScroll");
 
-		itemWarpStone = new ItemWarpStone();
-		GameRegistry.registerItem(itemWarpStone, "warpStone");
+        itemWarpStone = new ItemWarpStone();
+        GameRegistry.registerItem(itemWarpStone, "warpStone");
 
-		NetworkHandler.init();
+        NetworkHandler.init();
 
-		configuration = new Configuration(event.getSuggestedConfigurationFile());
-		config = new WaystoneConfig();
-		config.reloadLocal(configuration);
-		if(configuration.hasChanged()) {
-			configuration.save();
-		}
+        configuration = new Configuration(event.getSuggestedConfigurationFile());
+        config = new WaystoneConfig();
+        config.reloadLocal(configuration);
+        if (configuration.hasChanged()) {
+            configuration.save();
+        }
 
-		proxy.preInit(event);
-	}
+        proxy.preInit(event);
+    }
 
-	@Mod.EventHandler
-	public void init(FMLInitializationEvent event) {
-		FMLInterModComms.sendMessage("Waila", "register", "net.blay09.mods.waystones.compat.WailaProvider.register");
-	}
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent event) {
+        FMLInterModComms.sendMessage("Waila", "register", "net.blay09.mods.waystones.compat.WailaProvider.register");
+    }
 
-	@Mod.EventHandler
-	public void postInit(FMLPostInitializationEvent event) {
-		if(config.allowReturnScrolls) {
-			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(itemReturnScroll, 3), "GEG", "PPP", 'G', "nuggetGold", 'E', Items.ender_pearl, 'P', Items.paper));
-		}
-		if(config.lootReturnScrolls) {
-			// Item, min, max, weight
-			ChestGenHooks.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH)
-					.addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 3));
-			ChestGenHooks.getInfo(ChestGenHooks.MINESHAFT_CORRIDOR)
-					.addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 3));
-			ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST)
-					.addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 2, 3));
-			ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_DESERT_CHEST)
-					.addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 3));
-			ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_CHEST)
-					.addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 2));
-			ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_LIBRARY)
-					.addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 2));
-		}
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        if (config.allowReturnScrolls) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(itemReturnScroll, 3), "GEG", "PPP", 'G', "nuggetGold", 'E', Items.ender_pearl, 'P', Items.paper));
+        }
+        if (config.lootReturnScrolls) {
+            // Item, min, max, weight
+            ChestGenHooks.getInfo(ChestGenHooks.VILLAGE_BLACKSMITH)
+                .addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 3));
+            ChestGenHooks.getInfo(ChestGenHooks.MINESHAFT_CORRIDOR)
+                .addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 3));
+            ChestGenHooks.getInfo(ChestGenHooks.DUNGEON_CHEST)
+                .addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 2, 3));
+            ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_DESERT_CHEST)
+                .addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 3));
+            ChestGenHooks.getInfo(ChestGenHooks.PYRAMID_JUNGLE_CHEST)
+                .addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 2));
+            ChestGenHooks.getInfo(ChestGenHooks.STRONGHOLD_LIBRARY)
+                .addItem(new WeightedRandomChestContent(new ItemStack(itemReturnScroll), 1, 1, 2));
+        }
 
-		if(config.allowWarpStone) {
-			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(itemWarpStone), "DED", "EGE", "DED", 'D', "dyePurple", 'E', Items.ender_pearl, 'G', "gemEmerald"));
-		}
+        if (config.allowWarpStone) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(itemWarpStone), "DED", "EGE", "DED", 'D', "dyePurple", 'E', Items.ender_pearl, 'G', "gemEmerald"));
+        }
 
-		if(!config.creativeModeOnly) {
-			GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(blockWaystone), " S ", "SWS", "OOO", 'S', Blocks.stonebrick, 'W', itemWarpStone, 'O', Blocks.obsidian));
-		}
+        if (!config.creativeModeOnly) {
+            GameRegistry.addRecipe(new ShapedOreRecipe(new ItemStack(blockWaystone), " S ", "SWS", "OOO", 'S', Blocks.stonebrick, 'W', itemWarpStone, 'O', Blocks.obsidian));
+        }
 
-	}
-	public static WaystoneConfig getConfig() {
-		return config;
-	}
+    }
 
-	public static void setConfig(WaystoneConfig config) {
-		Waystones.config = config;
-	}
+    public static WaystoneConfig getConfig() {
+        return config;
+    }
+
+    public static void setConfig(WaystoneConfig config) {
+        Waystones.config = config;
+    }
 
 }
--- src/main/java/net/blay09/mods/waystones/client/gui/GuiWaystoneName.java
+++ src/main/java/net/blay09/mods/waystones/client/gui/GuiWaystoneName.java
@@ -15,78 +15,78 @@ import org.lwjgl.input.Keyboard;
 
 public class GuiWaystoneName extends GuiScreen {
 
-	private final TileWaystone tileWaystone;
-	private GuiTextField textField;
-	private GuiButton btnDone;
-	private GuiCheckBox chkGlobal;
+    private final TileWaystone tileWaystone;
+    private GuiTextField textField;
+    private GuiButton btnDone;
+    private GuiCheckBox chkGlobal;
 
-	public GuiWaystoneName(TileWaystone tileWaystone) {
-		this.tileWaystone = tileWaystone;
-	}
+    public GuiWaystoneName(TileWaystone tileWaystone) {
+        this.tileWaystone = tileWaystone;
+    }
 
-	@Override
-	@SuppressWarnings("unchecked")
-	public void initGui() {
-		String oldText = tileWaystone.getWaystoneName();
-		if(textField != null) {
-			oldText = textField.getText();
-		}
-		textField = new GuiTextField(fontRendererObj, width / 2 - 100, height / 2 - 20, 200, 20);
-		textField.setText(oldText);
-		textField.setFocused(true);
-		btnDone = new GuiButton(0, width / 2, height / 2 + 10, 100, 20, I18n.format("gui.done"));
-		buttonList.add(btnDone);
+    @Override
+    @SuppressWarnings("unchecked")
+    public void initGui() {
+        String oldText = tileWaystone.getWaystoneName();
+        if (textField != null) {
+            oldText = textField.getText();
+        }
+        textField = new GuiTextField(fontRendererObj, width / 2 - 100, height / 2 - 20, 200, 20);
+        textField.setText(oldText);
+        textField.setFocused(true);
+        btnDone = new GuiButton(0, width / 2, height / 2 + 10, 100, 20, I18n.format("gui.done"));
+        buttonList.add(btnDone);
 
-		chkGlobal = new GuiCheckBox(1, width / 2 - 100, height / 2 + 15, " " + I18n.format("gui.waystones:editWaystone.isGlobal"), WaystoneManager.getServerWaystone(tileWaystone.getWaystoneName()) != null);
-		if(!Minecraft.getMinecraft().thePlayer.capabilities.isCreativeMode) {
-			chkGlobal.visible = true;
-		}
-		buttonList.add(chkGlobal);
+        chkGlobal = new GuiCheckBox(1, width / 2 - 100, height / 2 + 15, " " + I18n.format("gui.waystones:editWaystone.isGlobal"), WaystoneManager.getServerWaystone(tileWaystone.getWaystoneName()) != null);
+        if (!Minecraft.getMinecraft().thePlayer.capabilities.isCreativeMode) {
+            chkGlobal.visible = true;
+        }
+        buttonList.add(chkGlobal);
 
-		Keyboard.enableRepeatEvents(true);
-	}
+        Keyboard.enableRepeatEvents(true);
+    }
 
-	@Override
-	public void onGuiClosed() {
-		Keyboard.enableRepeatEvents(false);
-	}
+    @Override
+    public void onGuiClosed() {
+        Keyboard.enableRepeatEvents(false);
+    }
 
-	@Override
-	protected void actionPerformed(GuiButton button) {
-		if(button == btnDone) {
-			NetworkHandler.channel.sendToServer(new MessageWaystoneName(new BlockPos(tileWaystone), textField.getText(), chkGlobal.isChecked()));
-			mc.displayGuiScreen(null);
-		}
-	}
+    @Override
+    protected void actionPerformed(GuiButton button) {
+        if (button == btnDone && !textField.getText().isEmpty()) {
+            NetworkHandler.channel.sendToServer(new MessageWaystoneName(new BlockPos(tileWaystone), textField.getText(), chkGlobal.isChecked()));
+            mc.displayGuiScreen(null);
+        }
+    }
 
-	@Override
-	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-		super.mouseClicked(mouseX, mouseY, mouseButton);
-		textField.mouseClicked(mouseX, mouseY, mouseButton);
-	}
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+        textField.mouseClicked(mouseX, mouseY, mouseButton);
+    }
 
-	@Override
-	protected void keyTyped(char typedChar, int keyCode) {
-		if(keyCode == Keyboard.KEY_RETURN) {
-			actionPerformed(btnDone);
-			return;
-		}
-		super.keyTyped(typedChar, keyCode);
-		textField.textboxKeyTyped(typedChar, keyCode);
-	}
+    @Override
+    protected void keyTyped(char typedChar, int keyCode) {
+        if (keyCode == Keyboard.KEY_RETURN) {
+            actionPerformed(btnDone);
+            return;
+        }
+        super.keyTyped(typedChar, keyCode);
+        textField.textboxKeyTyped(typedChar, keyCode);
+    }
 
-	@Override
-	public void updateScreen() {
-		textField.updateCursorCounter();
-	}
+    @Override
+    public void updateScreen() {
+        textField.updateCursorCounter();
+    }
 
-	@Override
-	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-		drawWorldBackground(0);
-		super.drawScreen(mouseX, mouseY, partialTicks);
+    @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+        drawWorldBackground(0);
+        super.drawScreen(mouseX, mouseY, partialTicks);
 
-		fontRendererObj.drawString(I18n.format("gui.waystones:editWaystone.enterName"), width / 2 - 100, height / 2 - 35, 0xFFFFFF);
-		textField.drawTextBox();
-	}
+        fontRendererObj.drawString(I18n.format("gui.waystones:editWaystone.enterName"), width / 2 - 100, height / 2 - 35, 0xFFFFFF);
+        textField.drawTextBox();
+    }
 
 }
